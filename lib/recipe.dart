bd7ba9b452b83abf6679ea9eb222552fbd453e5d
Jorge Atempa <atempaitm@gmail.com>
2019-04-03 12:07:17 -0700

    Replace property

--- lib/recipe.dart
+++ lib/recipe.dart
@@ -2,20 +2,20 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Recipe {
   final String name;
+  final String description;
   final String imageUrl;
   final String dietLabel;
-  final int servings;
   final List<dynamic> ingredients;
   final DocumentReference reference;
 
   Recipe.fromMap(Map<String, dynamic> map, {this.reference}) 
     : assert(map['title'] != null),
-      assert(map['servings'] != null),
+      assert(map['description'] != null),
       assert(map['image'] != null),
       assert(map['dietLabel'] != null),
       assert(map['ingredientLines'] != null),
       name = map['title'],
-      servings = map['servings'],
+      description = map['description'],
       imageUrl = map['image'],
       dietLabel = map['dietLabel'],
       ingredients = map['ingredientLines'];
@@ -24,5 +24,5 @@ class Recipe {
     : this.fromMap(snapshot.data, reference: snapshot.reference);
   
   @override
-  String toString() => "Recipe<$name:$servings>";
+  String toString() => "Recipe<$name>";
 }

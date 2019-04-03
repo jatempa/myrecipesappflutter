3721a48ba17a38a60d1555c21f325ecad5c69586
Jorge Atempa <atempaitm@gmail.com>
2019-04-03 10:15:39 -0700

    re-indent properties

--- lib/recipe.dart
+++ lib/recipe.dart
@@ -1,25 +1,28 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Recipe {
- final String name;
- final String imageUrl;
- final String dietLabel;
- final int servings;
- final DocumentReference reference;
+  final String name;
+  final String imageUrl;
+  final String dietLabel;
+  final int servings;
+  final List<dynamic> ingredients;
+  final DocumentReference reference;
 
- Recipe.fromMap(Map<String, dynamic> map, {this.reference})
+  Recipe.fromMap(Map<String, dynamic> map, {this.reference}) 
     : assert(map['title'] != null),
-    assert(map['servings'] != null),
-    assert(map['image'] != null),
-    assert(map['dietLabel'] != null),
-    name = map['title'],
-    servings = map['servings'],
-    imageUrl = map['image'],
-    dietLabel = map['dietLabel'];
-
- Recipe.fromSnapshot(DocumentSnapshot snapshot)
-     : this.fromMap(snapshot.data, reference: snapshot.reference);
-
- @override
- String toString() => "Recipe<$name:$servings>";
+      assert(map['servings'] != null),
+      assert(map['image'] != null),
+      assert(map['dietLabel'] != null),
+      assert(map['ingredientLines'] != null),
+      name = map['title'],
+      servings = map['servings'],
+      imageUrl = map['image'],
+      dietLabel = map['dietLabel'],
+      ingredients = map['ingredientLines'];
+  
+  Recipe.fromSnapshot(DocumentSnapshot snapshot)
+    : this.fromMap(snapshot.data, reference: snapshot.reference);
+  
+  @override
+  String toString() => "Recipe<$name:$servings>";
 }

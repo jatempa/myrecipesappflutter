f0a1ce5a991b1cf2e65339c8aad1e89f73dd698c
Jorge Atempa <atempaitm@gmail.com>
2019-04-03 12:25:50 -0700

    Refactor list

--- lib/recipe_list.dart
+++ lib/recipe_list.dart
@@ -32,7 +32,6 @@ class RecipeListState extends State<RecipeList> {
 
   Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
     return ListView(
-      padding: const EdgeInsets.only(top: 20.0),
       children: snapshot.map((data) => _buildListItem(context, data)).toList(),
     );
   }
@@ -40,26 +39,30 @@ class RecipeListState extends State<RecipeList> {
   Widget _buildListItem(BuildContext context, DocumentSnapshot snapshot) {
     final recipe = Recipe.fromSnapshot(snapshot);
 
-    return Card(
-      margin: EdgeInsets.all(8),
-      child: Column(
-        children: <Widget>[
-          ListTile(
-            contentPadding: EdgeInsets.only(top: 8, bottom: 8, left: 8, right: 8),
-            leading: Image.network(recipe.imageUrl, width: 50, fit: BoxFit.fill,),
-            title: Text(recipe.name, style: Styles.headerLarge,),
-            subtitle: Text(recipe.dietLabel, style: Styles.subHeaderLarge,),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => RecipeDetail(recipe: snapshot,)
-                ),
-              );
-            },
-          )          
-        ],
-      )
+    return Column(
+      children: [
+        ListTile(
+          contentPadding: EdgeInsets.only(left: 8, bottom: 10),
+          leading: Image.network(recipe.imageUrl, height: 70, width: 70),
+          title: Text(recipe.name, style: Styles.headerLarge,),
+          subtitle: RichText(
+            maxLines: 3,
+            text: TextSpan(
+              text: recipe.description,
+              style: Styles.textDefault,
+            ),
+          ),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => RecipeDetail(snapshot)
+              ),
+            );
+          },
+        ),
+        Divider(height: 30,),
+      ],
     );
   }  
 }

ff10fc2fe521f73a2d6ef62d869452e8b28d2221
Jorge Atempa <atempaitm@gmail.com>
2019-04-03 12:25:27 -0700

    Change font size

--- lib/styles.dart
+++ lib/styles.dart
@@ -1,34 +1,20 @@
 import 'package:flutter/material.dart';
 
 class Styles {
-  static const _textSizeLarge = 18.0;
-  static const _textSizeDefault = 12.0;
-  static final Color _textColorStrong = _hexToColor('000000');
-  static final Color _textColorDefault = _hexToColor('666666');
+  static const _textSizeLarge = 16.0;
+  static const _textSizeDefault = 14.0;
   static final String _fontNameHeader = 'JosefinSans';
-  static final String _fontNameDefault = 'Quicksand';
-  static final navBarTitle = TextStyle(
-    fontFamily: _fontNameDefault,
-  );
+
   static final headerLarge = TextStyle(
     fontFamily: _fontNameHeader,
-    fontWeight: FontWeight.bold,
     fontSize: _textSizeLarge,
-    color: _textColorStrong,
-  );
-  static final subHeaderLarge = TextStyle(
-    fontFamily: _fontNameHeader,
-    fontStyle: FontStyle.italic,
-    fontSize: _textSizeLarge,
-    color: _textColorStrong,
-  );
-  static final textDefault = TextStyle(
-    fontFamily: _fontNameDefault,
-    fontSize: _textSizeDefault,
-    color: _textColorDefault,
+    color: Color.fromRGBO(0, 0, 0, 1),
   );
 
-  static Color _hexToColor(String code) {
-    return Color(int.parse(code.substring(0, 6), radix: 16) + 0xFF000000);
-  }
+  static final textDefault = TextStyle(
+    fontFamily: _fontNameHeader,
+    fontStyle: FontStyle.italic,
+    fontSize: _textSizeDefault,
+    color: Color.fromRGBO(102, 102, 102, 1),
+  );
 }

a75e8925fa024d5e73b4e9d5730281c4d4646cd0
Jorge Atempa <atempaitm@gmail.com>
2019-04-05 00:05:01 -0700

    Move dart class to specific folders

--- lib/recipe_detail.dart
+++ lib/recipe_detail.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-import 'recipe.dart';
+import 'model/recipe.dart';
 
 class RecipeDetail extends StatefulWidget {
   final DocumentSnapshot snapshot;

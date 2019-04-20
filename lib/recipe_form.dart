577f09d961708edf24421835385015eff6617e5f
Jorge Atempa <atempaitm@gmail.com>
2019-04-19 21:42:02 -0700

    Save recipe

--- lib/recipe_form.dart
+++ lib/recipe_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class RecipeForm extends StatefulWidget {
   @override
@@ -8,96 +9,81 @@ class RecipeForm extends StatefulWidget {
 class RecipeFormState extends State<RecipeForm> {
   final List<String> labels = ['High-Fat', 'Low-Fat', 'Low-Carb', 'Low-Sodium', 'Medium-Carb', 'Vegetarian', 'Balanced'];
   final _formKey = GlobalKey<FormState>();
-  String name;
-  String description;
-  String urlImage;
-  String dietLabelSelected = 'High-Fat';
+  Map<String, String> recipeValues = {
+    'title': '',
+    'description': '',
+    'image': '',
+    'dietLabel': 'High-Fat'
+  };
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: Text('Create a recipe'),
-      content: Form(
-        key: _formKey,
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Enter the recipe name',
-                  labelText: 'Name',
-                ),
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter a name';
-                  }
-                },
-                onSaved: (value) {
-                  setState(() => name = value);
-                }
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Enter the recipe description',
-                  labelText: 'Description',
-                ),
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter a description';
-                  }
-                },                
-                onSaved: (value) {
-                  setState(() => description = value);
-                }
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Enter the recipe url image',
-                  labelText: 'Image',
-                ),
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter a url image';
-                  }
-                },                
-                onSaved: (value) {
-                  setState(() => urlImage = value);
-                }
-              ),
-              DropdownButtonFormField<String>(
-                value: dietLabelSelected,
-                decoration: InputDecoration(
-                  hintText: 'Enter the diet label',
-                  labelText: 'Diet Label',
-                ),
-                items: labels.map((label) => DropdownMenuItem(
-                  child: Text(label),
-                  value: label,
-                )).toList(),
-                onChanged: (value) {
-                  setState(() => dietLabelSelected = value);
-                }
-              ),
-            ]
-          )
-        ),
-      ),
+      content: buildRecipeForm(),
       actions: <Widget>[
         FlatButton(
           child: Text('Save'),
-          onPressed: () {
-            if (_formKey.currentState.validate()) {
-              _formKey.currentState.save();
-              print("$name");
-              print("$description");
-              print("$urlImage");
-              print("$dietLabelSelected");
-              Navigator.of(context).pop();
-            }
-          },
+          onPressed: () => createRecipe(),
         ),
       ]
     );
+  }
+
+  Form buildRecipeForm() {
+    return Form(
+      key: _formKey,
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            buildTextFormField('Enter the recipe name', 'Name', 'a name', 'name'),
+            buildTextFormField('Enter the recipe description', 'Description', 'the description', 'description'),
+            buildTextFormField('Enter the recipe url image', 'Image', 'url image', 'image'),
+            DropdownButtonFormField<String>(
+              value: recipeValues['dietLabel'],
+              decoration: InputDecoration(
+                hintText: 'Enter the diet label',
+                labelText: 'Diet Label',
+              ),
+              items: labels.map((label) => DropdownMenuItem(
+                child: Text(label),
+                value: label,
+              )).toList(),
+              onChanged: (value) {
+                setState(() => recipeValues['dietLabel'] = value);
+              }
+            ),
+          ]
+        )
+      ),
+    );
+  }
+
+  TextFormField buildTextFormField(String hint, String label, String error, String key) {
+    return TextFormField(
+      decoration: InputDecoration(
+        hintText: hint,
+        labelText: label,
+      ),
+      validator: (value) {
+        if (value.isEmpty) {
+          return "Please enter $error";
+        }
+      },
+      onSaved: (value) {
+        setState(() => recipeValues[key] = value);
+      }
+    );    
+  }
+ 
+  void createRecipe() async {
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+      print("$recipeValues");
+      DocumentReference ref = await Firestore.instance.collection('recipes').add(recipeValues);
+      print(ref.documentID);
+      Navigator.of(context).pop();
+    }    
   }
 }
